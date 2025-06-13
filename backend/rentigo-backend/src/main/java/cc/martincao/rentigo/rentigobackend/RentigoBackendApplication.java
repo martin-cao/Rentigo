2d5d90e82a6783187e5b6e7629c07484e076adbe
martin-cao <martincao119@icloud.com>
2025-06-13 16:32:53 +0800

    Initial commit

---
+++ backend/rentigo-backend/src/main/java/cc/martincao/rentigo/rentigobackend/RentigoBackendApplication.java
@@ -0,0 +1,13 @@
+package cc.martincao.rentigo.rentigobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentigoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentigoBackendApplication.class, args);
+	}
+
+}
